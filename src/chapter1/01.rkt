681d743e7bddcfd910751825a48e82a1841465c3
Sementsov Nikita <nikita.sementsov@abbyy.com>
2021-07-01 11:41:08 +0300

    add exercise texts

--- src/chapter1/01.rkt
+++ src/chapter1/01.rkt
@@ -1,5 +1,9 @@
 #lang sicp
 
+; Below is a sequence of expressions. What is
+; the result printed by the interpreter in response to each expression? Assume that the sequence is to be evaluated in
+; the order in which it is presented.
+
 10                                                          ; 10
 (+ 5 3 4)                                                   ; 12
 (- 9 1)                                                     ; 8
--- src/chapter1/02.rkt
+++ src/chapter1/02.rkt
@@ -2,6 +2,10 @@
 
 (#%require rackunit)
 
+; Exercise #1.2
+; Translate the following expression into prefix form:
+; (5 + 4 + (2 − (3 − (6 + 4 / 5 )))) / (3 * (6 − 2) * (2 − 7))
+
 (define solution
         (/ (+ 5
               4
